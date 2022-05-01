a9cfbf572ae0c6b564cd5ce9ecc41ccf19e68360
Mabel Quek <mxquek1@gmail.com>
2022-04-30 19:40:42 -0500

    Implemented MissionsController GetByAgency() & GetByAgent()

--- FieldAgent.Web/Controllers/MissionsController.cs
+++ FieldAgent.Web/Controllers/MissionsController.cs
@@ -139,6 +139,76 @@ namespace FieldAgent.Web.Controllers
                 return BadRequest(ModelState);
             }
         }
+
+        [HttpGet]
+        [Route("agency/{agencyId}")]
+        public IActionResult GetByAgency(int agencyId)
+        {
+            var result = _MissionRepository.GetByAgency(agencyId);
+            if (result.Success)
+            {
+                if(result.Data.Count == 0)
+                {
+                    return NotFound($"No missions found for Agency {agencyId}");
+                }
+
+                return Ok
+                (
+                    result.Data.Select
+                    (mission => new MissionModel()
+                    {
+                        MissionId = mission.MissionId,
+                        CodeName = mission.CodeName,
+                        StartDate = mission.StartDate,
+                        ProjectedEndDate = mission.ProjectedEndDate,
+                        ActualEndDate = mission.ActualEndDate,
+                        OperationalCost = mission.OperationalCost,
+                        Notes = mission.Notes,
+
+                        AgencyId = mission.AgencyId
+                    })
+                );
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("agent/{agentId}")]
+        public IActionResult GetByAgent(int agentId)
+        {
+            var result = _MissionRepository.GetByAgent(agentId);
+            if (result.Success)
+            {
+                if (result.Data.Count == 0)
+                {
+                    return NotFound($"No missions found for Agent {agentId}");
+                }
+
+                return Ok
+                (
+                    result.Data.Select
+                    (mission => new MissionModel()
+                    {
+                        MissionId = mission.MissionId,
+                        CodeName = mission.CodeName,
+                        StartDate = mission.StartDate,
+                        ProjectedEndDate = mission.ProjectedEndDate,
+                        ActualEndDate = mission.ActualEndDate,
+                        OperationalCost = mission.OperationalCost,
+                        Notes = mission.Notes,
+
+                        AgencyId = mission.AgencyId
+                    })
+                );
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
     }
 
 
